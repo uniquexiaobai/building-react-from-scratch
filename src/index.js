68522751f3f53ecae092d08787ee23094450ae93
uniquexiaobai <uniquexiaobai@gmail.com>
2019-12-10 22:23:16 +0800

    reconciliation

--- src/index.js
+++ src/index.js
@@ -38,16 +38,65 @@ function createDom(fiber) {
 }
 
 function commitRoot() {
+	deletions.forEach(commitWork);
 	commitWork(wipRoot.child);
 	currentRoot = wipRoot;
 	wipRoot = null;
+}
+
+const isEvent = key => key.startsWith('on');
+const isProperty = key => key !== 'children' && !isEvent(key);
+const isNew = (prev, next) => key => prev[key] !== next[key];
+const isGone = (prev, next) => key => !(key in next);
+
+function updateDom(dom, prevProps, nextProps) {
+	// remove old or changed event listeners
+	Object.keys(prevProps)
+		.filter(isEvent)
+		.filter(key => !(key in nextProps) || isNew(prevProps, nextProps(key)))
+		.forEach(name => {
+			const eventType = name.toLowerCase.slice(2);
+			dom.removeEventListener(eventType, prevProps[name]);
+		});
+
+	// remove old props
+	Object.keys(prevProps)
+		.filter(isProperty)
+		.filter(isGone(prevProps, nextProps))
+		.forEach(name => {
+			dom[name] = '';
+		});
+
+	// set new or changed props
+	Object.keys(prevProps)
+		.filter(isProperty)
+		.filter(isNew(prevProps, nextProps))
+		.forEach(name => {
+			dom[name] = nextProps[name];
+		});
+
+	// add event listeners
+	Object.keys(nextProps)
+		.filter(isEvent)
+		.filter(isNew(prevProps, nextProps))
+		.forEach(name => {
+			const eventType = name.toLowerCase().slice(2);
+			dom.addEventListener(eventType, nextProps[name]);
+		});
 }
 
 function commitWork(fiber) {
 	if (!fiber) return;
 
 	const domParent = fiber.parent.dom;
-	domParent.appendChild(fiber.dom);
+	if (fiber.effectTag === 'PLACEMENT' && fiber.dom != null) {
+		domParent.appendChild(fiber.dom);
+	} else if (fiber.effectTag === 'UPDATE' && fiber.dom != null) {
+		updateDom(fiber.dom, fiber.alternate.props, fiber.props);
+	} else if (fiber.effectTag === 'DELETION') {
+		domParent.removeChild(fiber.dom);
+	}
+
 	commitWork(fiber.child);
 	commitWork(fiber.sibling);
 }
@@ -60,13 +109,14 @@ function render(element, container) {
 		},
 		alternate: currentRoot,
 	};
-
+	deletions = [];
 	nextUnitOfWork = wipRoot;
 }
 
 let nextUnitOfWork = null;
 let currentRoot = null;
 let wipRoot = null;
+let deletions = null;
 
 requestIdleCallback(workLoop);
 
@@ -108,17 +158,39 @@ function performUnitOfWork(fiber) {
 
 function reconcileChildren(wipFiber, elements) {
 	let index = 0;
+	let oldFiber = wipFiber.alternate && wipFiber.alternate.child;
 	let prevSibling = null;
 
-	while (index < elements.length) {
+	while (index < elements.length || oldFiber != null) {
 		const element = elements[index];
+		let newFiber = null;
 
-		const newFiber = {
-			type: element.type,
-			props: element.props,
-			parent: wipFiber,
-			dom: null,
-		};
+		const sameType = oldFiber && element && element.type === oldFiber.type;
+
+		if (sameType) {
+			newFiber = {
+				type: oldFiber.type,
+				props: element.props,
+				dom: oldFiber.dom,
+				parent: wipFiber,
+				alternate: oldFiber,
+				effectTag: 'UPDATE',
+			};
+		}
+		if (element && !sameType) {
+			newFiber = {
+				type: element.type,
+				props: element.props,
+				dom: null,
+				parent: wipFiber,
+				alternate: null,
+				effectTag: 'PLACEMENT',
+			};
+		}
+		if (oldFiber && !sameType) {
+			oldFiber.effectTag = 'DELETION';
+			deletions.push(oldFiber);
+		}
 
 		if (index === 0) {
 			wipFiber.child = newFiber;
